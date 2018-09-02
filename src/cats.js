889314ecee8fc235566b7c31746e79688ce740c1
Briana Dacoscos <briana.dacoscos@gmail.com>
2018-09-02 16:21:25 -0700

    update cat properties and presentation

--- src/cats.js
+++ src/cats.js
@@ -2,32 +2,38 @@ const cats = {
   cat1: {
     name: "Cuddles",
     image: "/images/gray2.jpg",
-    price: 2000
+    price: 2000,
+    color: "gray"
   },
   cat2: {
     name: "Snowball",
     image: "/images/angora.jpg",
-    price: 3000
+    price: 3000,
+    color: "white"
   },
   cat3: {
     name: "Jimothy",
     image: "/images/persian.jpg",
-    price: 5000
+    price: 5000,
+    color: "orange"
   },
   cat4: {
-    name: "Cat",
+    name: "Catty",
     image: "/images/gray.jpg",
-    price: 3500
+    price: 3500,
+    color: "gray"
   },
   cat5: {
     name: "Spoon",
     image: "/images/black.jpg",
-    price: 3500
+    price: 3500,
+    color: "black"
   },
   cat6: {
     name: "Mr. Personality",
     image: "/images/orange.jpg",
-    price: 4250
+    price: 4250,
+    color: "orange"
   }
 };
 
